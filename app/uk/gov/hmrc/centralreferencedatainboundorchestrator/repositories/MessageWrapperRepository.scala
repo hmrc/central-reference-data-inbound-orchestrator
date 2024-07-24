ee913cdb17b1fe8a7e21e10bf33d238a7f1d104c
Ali Mahmood <ali.mahmood@digital.hmrc.gov.uk>
2024-07-24 13:08:03 +0100

    Adding mongo schema and functions for message wrapper

---
+++ app/uk/gov/hmrc/centralreferencedatainboundorchestrator/repositories/MessageWrapperRepository.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.centralreferencedatainboundorchestrator.repositories
+
+import com.google.inject.{Inject, Singleton}
+import org.mongodb.scala.model.Filters.equal
+import org.mongodb.scala.model.{Filters, IndexModel, IndexOptions, Indexes, UpdateOptions, Updates}
+import play.api.Logging
+import uk.gov.hmrc.centralreferencedatainboundorchestrator.config.AppConfig
+import uk.gov.hmrc.centralreferencedatainboundorchestrator.models.MessageWrapper
+import uk.gov.hmrc.mongo.MongoComponent
+import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
+import org.mongodb.scala.*
+
+import java.time.Instant
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class MessageWrapperRepository @Inject()(
+                                     mongoComponent: MongoComponent,
+                                     appConfig: AppConfig
+                                   )(implicit ec: ExecutionContext) extends PlayMongoRepository[MessageWrapper](
+  collectionName = "message-wrapper",
+  mongoComponent = mongoComponent,
+  domainFormat = MessageWrapper.mongoFormat,
+  indexes = Seq(
+    IndexModel(
+      Indexes.ascending("uid"),
+      IndexOptions().name("uid").unique(true)
+    )
+  ),
+  replaceIndexes = true
+) with Logging with MessageWrapperRepoTrait {
+
+  def insert(uid: String,
+             payload: String,
+             status: String)
+            (implicit ec: ExecutionContext): Future[Boolean] = {
+    logger.info(s"Inserting a message wrapper in $collectionName table with uid: $uid")
+    collection.insertOne(MessageWrapper(uid, payload, status))
+      .head()
+      .map(_ =>
+        logger.info(s"Inserted a message wrapper in $collectionName table with uid: $uid")
+          true
+        )
+      .recoverWith {
+        case e =>
+          logger.info(s"failed to insert message wrapper with uid: $uid into $collectionName table with ${e.getMessage}")
+          Future.successful(false)
+      }
+  }
+
+  def findByUid(uid: String)(implicit ec: ExecutionContext): Future[Option[MessageWrapper]] =
+    collection.find(Filters.equal("uid", uid))
+      .headOption()
+
+  def updateStatus(uid: String, status: String)(implicit ec: ExecutionContext): Future[Boolean] =
+    collection.updateOne(
+      equal("uid", uid),
+      Updates.combine(
+        Updates.set("status", status),
+        Updates.set("lastUpdatedTimestamp", Instant.now())
+      ),
+      UpdateOptions().upsert(true)
+    ).toFuture()
+      .map(_ => true)
+      .recoverWith {
+          case e =>
+            logger.info(s"failed to update message wrappers status with uid: $uid & status: $status in $collectionName table with ${e.getMessage}")
+            Future.successful(false)
+      }
+
+}
