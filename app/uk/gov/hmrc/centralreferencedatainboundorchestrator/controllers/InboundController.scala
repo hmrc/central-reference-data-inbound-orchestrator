233649a739235e9cc4895eaff31ace78b4474a89
Ali Mahmood <ali.mahmood@digital.hmrc.gov.uk>
2024-09-17 10:16:19 +0100

    Removing log info

--- app/uk/gov/hmrc/centralreferencedatainboundorchestrator/controllers/InboundController.scala
+++ app/uk/gov/hmrc/centralreferencedatainboundorchestrator/controllers/InboundController.scala
@@ -48,9 +48,7 @@ class InboundController @Inject()(
       inboundControllerService.processMessage(request.body) transform {
         case Success(_) => Success(Accepted)
         case Failure(err: Throwable) => err match
-          case InvalidXMLContentError(_) =>
-            logger.info("Failed to validate schema of message - potentially an error report")
-            Success(BadRequest)
+          case InvalidXMLContentError(_) => Success(BadRequest)
           case MongoReadError(_) | MongoWriteError(_) => Success(InternalServerError)
           case _ => Success(InternalServerError)
       }
