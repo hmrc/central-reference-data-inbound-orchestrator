cbfe1874d6cdf4f6452774ba26d1d4bd764e8ec3
thabeep <173276578+thabeep@users.noreply.github.com>
2024-10-15 15:35:54 +0100

    CRDL-165: Update Orchestrator to send correct message

--- app/uk/gov/hmrc/centralreferencedatainboundorchestrator/connectors/EisConnector.scala
+++ app/uk/gov/hmrc/centralreferencedatainboundorchestrator/connectors/EisConnector.scala
@@ -24,6 +24,9 @@ import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
 import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.http.HttpReads.Implicits.*
 
+import java.time.{ZoneOffset, ZonedDateTime}
+import java.time.format.DateTimeFormatter
+import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 import scala.xml.NodeSeq
 
@@ -34,11 +37,15 @@ class EisConnector @Inject()(httpClient: HttpClientV2, appConfig: AppConfig):
                                     hc: HeaderCarrier
   ): Future[Boolean] =
     val url = s"${appConfig.eisUrl}${appConfig.eisPath}"
+    val iso8601DateTimeFormat = DateTimeFormatter.ofPattern("EEE dd MMM yyyy HH:mm:ss 'GMT'")
     httpClient
       .post(url"$url")
       .setHeader("Accept" -> "application/xml")
-      .setHeader("Content-Type" -> "application/xml")
+      .setHeader("Content-Type" -> "application/xml;charset=UTF-8")
       .setHeader("Authorization" -> appConfig.eisBearerToken)
+      .setHeader("X-Forwarded-Host" -> "central-reference-data-inbound-orchestrator")
+      .setHeader("X-Correlation-Id" -> UUID.randomUUID().toString)
+      .setHeader("Date" -> iso8601DateTimeFormat.format(ZonedDateTime.now(ZoneOffset.UTC)))
       .withBody(body)
       .execute[HttpResponse]
       .map(_.status == ACCEPTED)
