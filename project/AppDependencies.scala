882504d3515a2320f799574f9a9f68461cab1fc4
David Gregory <2992938+DavidGregory084@users.noreply.github.com>
2025-04-29 15:29:48 +0100

    Update sbt and dependency versions as suggested by PR bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,21 +1,21 @@
 import play.core.PlayVersion
-import play.sbt.PlayImport._
+import play.sbt.PlayImport.*
 import sbt.Keys.libraryDependencies
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
   private val bootstrapVersion = "9.11.0"
-  private val hmrcMongoVersion = "2.5.0"
+  private val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"          % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-work-item-repo-play-30"  % hmrcMongoVersion
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30"         % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-work-item-repo-play-30" % hmrcMongoVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"            % hmrcMongoVersion
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion
   ).map(_ % Test)
 
   val it: Seq[ModuleID] = Seq.empty
